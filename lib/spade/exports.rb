26474da5ca6e42958a5a21ef7681f76b606ffe2b
Charles Jolley <charles@sproutcore.com>
2010-12-29 02:10:17 -0800

    Auto-subclass to expose a context property on objects exposed as ruby modules

--- lib/spade/exports.rb
+++ lib/spade/exports.rb
@@ -20,12 +20,49 @@ module Spade
   
   class Exports
     
-    include Namespace
-    
     attr_reader :context
     
     def initialize(ctx)
       @context = ctx
+    end
+
+    def [](name)
+      
+      begin
+        if self.class.const_defined?(name)
+          ret = self.class.const_get(name)
+          
+          # If we are returning a class, create a custom subclass the first 
+          # time that also exposes the current context.
+          if ret.instance_of? Class
+            @klass_cache ||= {}
+            unless @klass_cache[name]
+
+              proc1 = proc { @context } 
+              @klass_cache[name] = Class.new(ret) do
+                
+                @context = proc1.call
+
+                def self.context
+                  @context
+                end
+                
+              end
+
+            end
+            
+            @klass_cache[name]
+          else
+            ret
+          end
+            
+        else
+          yield
+        end
+        
+      rescue NameError => e
+        yield
+      end
     end
     
   end
