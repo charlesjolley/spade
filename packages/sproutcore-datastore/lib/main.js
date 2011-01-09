ced58cc9f29b0bdd6da4663f4d60992d114382d7
Charles Jolley <charles@sproutcore.com>
2011-01-09 10:11:42 -0800

    Partial conversion of datastore framework.  Not working yet.

---
+++ packages/sproutcore-datastore/lib/main.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2010 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+require('./core');
+require('./store');
+require('./nested_store');
+require('./query');
+require('./record');
+require('./data_source');
