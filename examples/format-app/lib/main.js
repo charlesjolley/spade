62cbcfc01e1706a1bd284293ebfd6d4bedf5e6bf
Charles Jolley <charles@sproutcore.com>
2011-01-12 18:04:50 -0800

    Added coffee-script example

--- examples/format-app/lib/main.js
+++ examples/format-app/lib/main.js
@@ -2,4 +2,4 @@
 exports.README = require('format-app/~resources/README');
 exports.data   = require('format-app/~resources/config');
 
-exports.LOCD   = require('format-app/~lproj/FILE');
+require('./hello');
